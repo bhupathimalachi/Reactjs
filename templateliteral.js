6ae0bb08a3a05bab6f1478d58f6376fcccbbeeea
bhupathimalchi <https://github.com/bhupathimalachi/Reactjs>
2021-08-22 22:17:39 +0530

    javanotes

--- templateliteral.js
+++ templateliteral.js
@@ -13,11 +13,33 @@ console.log(typeof Symbol(100));//symbol  to provide a security for that number
 var sub="reactjs";
 var wish=`welcome to ${sub}`;
 console.log(wish);
-/* */
+
 
 var tbl_name="employess";
 var sal=50000;
 var sql_query=`select * from ${tbl_name} where sal > ${sal}`;
 console.log(sql_query);
+
+var usernaem="bhupathi";
+var upwd="malachi"
+var login_query=`select * from ${tbl_name} where usernaem="${usernaem}" and upwd="${upwd}`;
+console.log(login_query);
+
+
+var sub_one="react js";
+var sub_two="node js";
+var sub_three="magoDB";
+var mearn=`
+           MERN stact Development
+           **********************
+           front end ==>${sub_one}
+           back end  ==>${sub_two}
+           data base ==>${sub_three}`;
+console.log(mearn);// templet litterial is called as backtrick 
+
+
+
+
+
 
 
