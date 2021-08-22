4d8506122ed9e12d388f135d9f0ca4d4a1c9c99e
bhupathimalchi <https://github.com/bhupathimalachi/Reactjs>
2021-08-22 21:47:43 +0530

    javascriptnotes

---
+++ templateliteral.js
@@ -0,0 +1,9 @@
+var sub="reactjs";
+var wish=`welcome to ${sub}`;
+console.log(wish);
+
+
+var tbl_name="employess";
+var sal=50000;
+var sql_query=`select * from ${tbl_name} where sal > ${sal}`;
+console.log(sql_query);
